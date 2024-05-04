ac0c360dfae702cb0507eac4ed5a0c7816754cf2
xufei <xufei@xufei-VMware-Ubuntu24.04LTS>
2024-05-04 21:02:28 +0800

    feature: add generate password and check password strength

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,5 @@ mod opts;
 mod process;
 
 pub use opts::{Opts, SubCommand};
-pub use process::process_csv;
+pub use process::csv_convert::process_csv;
+pub use process::gen_pass::process_genpass;
--- src/opts.rs
+++ src/opts.rs
@@ -14,8 +14,10 @@ pub struct Opts {
 
 #[derive(Debug, Subcommand)]
 pub enum SubCommand {
-    #[command(name = "csv", about = "Show CSV, and convert CSV to other formaty")]
+    #[command(name = "csv", about = "Show CSV, and convert CSV to other format")]
     Csv(CsvOpts),
+    #[command(name = "genpass", about = "Generate a random password")]
+    GenPass(GenPassOpts),
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -41,6 +43,24 @@ pub struct CsvOpts {
 
     #[arg(long, default_value_t = true)]
     pub header: bool,
+}
+
+#[derive(Debug, Parser)]
+pub struct GenPassOpts {
+    #[arg(short, long, default_value_t = 16)]
+    pub length: u8,
+
+    #[arg(long, default_value_t = true)]
+    pub uppercase: bool,
+
+    #[arg(long, default_value_t = true)]
+    pub lowercase: bool,
+
+    #[arg(long, default_value_t = true)]
+    pub number: bool,
+
+    #[arg(long, default_value_t = true)]
+    pub symbol: bool,
 }
 
 fn verify_input_file(filename: &str) -> Result<String, String> {
